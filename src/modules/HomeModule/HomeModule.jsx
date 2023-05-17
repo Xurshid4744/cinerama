a3e2c5f032688cf44a2583891cf2296582177d6e
Xurshid4744 <nematoff4744@gmail.com>
2023-05-17 10:43:16 +0500

    fixed

--- src/modules/HomeModule/HomeModule.jsx
+++ src/modules/HomeModule/HomeModule.jsx
@@ -3,17 +3,17 @@ import FilmCard from "components/Cards/FilmCard";
 import CardSwiper from "components/Swiper/CardSwiper";
 import MainSwiper from "components/Swiper/MainSwiper/MainSwiper";
 import React from "react";
-import style from "./index.module.scss"
+import style from "./index.module.scss";
 const HomeModule = () => {
   return (
     <>
       <MainSwiper />
-      <CardSwiper name={"Сериалы"} title={"series"}/>
-      {/* <CollectionCard/> */}
-      {/* <CardSwiper name={"Фильмы"}/>
-      <CardSwiper name={"Мультфильмы"}/>
-      <CardSwiper name={"Узбеккино"}/>
-      <CardSwiper name={"Аниме"}/> */}
+      <CardSwiper name={"Сериалы"} title={"series"} />
+      <CollectionCard />
+      <CardSwiper name={"Фильмы"} />
+      <CardSwiper name={"Мультфильмы"} />
+      <CardSwiper name={"Узбеккино"} />
+      <CardSwiper name={"Аниме"} />
     </>
   );
 };
